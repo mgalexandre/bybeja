290de8a33662b1d70c3a217c12567efa142dc9d1
mgalexandre <xanitoalexandre@gmail.com>
2025-09-16 15:54:16 +0100

    feat/ added photos and changed the hero a bit

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,31 +1,26 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-[url('/hero-bg-cake-2.jpg')] bg-cover bg-center">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-3xl leading-10 text-white font-normal"
           >
-            Well done! You have successfully deployed your Medusa 2.0 store on Railway!
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Need help customizing your store?
+            Produtos de qualidade para todos os cake designers, do principiante
+            ao profissional temos tudo o que precisas para levar a tua arte mais
+            longe.
           </Heading>
         </span>
-        <a
-          href="https://funkyton.com/medusajs-2-0-is-finally-here/"
-          target="_blank"
-        >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
+        <a href="/store">
+          <button
+            className="bg-white text-black px-4 py-2 rounded-md"
+            type="button"
+          >
+            Ver produtos
+          </button>
         </a>
       </div>
     </div>

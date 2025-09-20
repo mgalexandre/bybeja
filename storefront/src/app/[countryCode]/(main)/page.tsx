a5a395dc475ff5489fd2212ad00ef40cbc5bf904
oxano <99873300+mgalexandre@users.noreply.github.com>
2025-09-20 14:16:21 +0100

    Atualizar o page.tsx

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -5,7 +5,7 @@ import Hero from "@modules/home/components/hero"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "ByBeja",
   description:
     "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
 }
